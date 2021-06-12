2127992b18341855db7954a8f9fde0c45bce29ff
GopiPrudhvi <gopiprudhvi@moneytap.com>
2021-06-13 02:38:30 +0530

    added the room db.. will have to test it.. by setting up the api call data into the data base table

--- byjusassignment/app/src/main/java/com/byjus/assignment/byjusAssignment/fragment/NewsFeedFragment.kt
+++ byjusassignment/app/src/main/java/com/byjus/assignment/byjusAssignment/fragment/NewsFeedFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.byjus.assignment.byjusAssignment.MainActivity
 import com.byjus.assignment.byjus_assignment.R
@@ -30,8 +32,8 @@ class NewsFeedFragment(): Fragment() {
     private var newsList: NewsListt.NewsList? = null
     private var newsDao: NewsArticlesDao? = null
     private var newsDisplayAdapter: NewsDisplayAdapter? = null
-    val newsFeedViewModel: NewsFeedViewModel by viewModels()
-    private lateinit var newsFVmodel: NewsFeedViewModel
+    private lateinit var newsFeedViewModel: NewsFeedViewModel
+//    private lateinit var newsFVmodel: NewsFeedViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,11 +42,21 @@ class NewsFeedFragment(): Fragment() {
     ): View? {
         viewBinding = FragmentNewsFeedBinding.inflate(layoutInflater)
         newsDao = NewsArticlesDatabase.getInstance(requireContext()).newsArticlesDao
+        val application = requireNotNull(activity).application
 //        val newsFeedViewModel: NewsFeedViewModel by viewModels{ NewsFeedViewModelFactory(newsDao!!, requireNotNull(activity).application)}
         //viewBinding.newsFeedViewModel = newsFeedViewModel
-        newsFVmodel = newsFeedViewModel
+//        newsFVmodel = newsFeedViewModel
 
 //        viewBinding.lifecycleOwner = this
+//        val newsFeedViewModel: NewsFeedViewModel by viewModels()
+//        newsFeedViewModel.getArticles("us","politics", API_KEY)
+        //initObservers(newsFeedViewModel)
+//        newsFVmodel = newsFeedViewModel
+
+        val viewModelProvider = NewsFeedViewModelFactory(newsDao!!, application)
+        newsFeedViewModel = ViewModelProviders.of(this, viewModelProvider).get(NewsFeedViewModel::class.java)
+        viewBinding.newsFeedViewModel = newsFeedViewModel
+        viewBinding.lifecycleOwner = this
         return view
     }
 
@@ -56,51 +68,54 @@ class NewsFeedFragment(): Fragment() {
 //                mutableListOf((NewsList.Article(NewsList.Source(" a"," b"),"a","b","","c "," d","e "," f")),
 //                        (NewsList.Article(NewsList.Source(" a"," b"),"a","b","","c "," d","e "," f"))))
         //newsFeedViewModel.getArticles("us","politics", API_KEY)
-        //getNewsList()
-        newsFVmodel.getArticles("us","politics", API_KEY)
-        initObservers()
+
+//        newsFVmodel.getArticles("us","politics", API_KEY)
+//        val newsFeedViewModel: NewsFeedViewModel by viewModels()
+//        newsFeedViewModel.getArticles("us","politics", API_KEY)
+        getNewsList()
+        //initObservers(newsFeedViewModel)
     }
 
     private fun getNewsList() {
-//        val call: Call<NewsListt.NewsList> = ApiClient.getClient().create(ApiInterface::class.java)
-//                        .getLatestNews("us","politics", API_KEY)
-//        call.enqueue(object : Callback<NewsListt.NewsList>{
-//            override fun onResponse(call: Call<NewsListt.NewsList>, response: Response<NewsListt.NewsList>) {
-//                newsList = response.body()
-//                //newsDisplayAdapter?.newsList = newsList!!
-////                newsDisplayAdapter.notifyDataSetChanged()
-//                newsDisplayAdapter = NewsDisplayAdapter(newsList!!, requireContext()) {
-//                    val bundle = Bundle()
-//                    bundle.putSerializable(NewsViewFragment.NEWS_LIST_ITEM, it)
-//                    (activity as MainActivity).supportFragmentManager.commit {
-//                        replace(
-//                            R.id.fragment_container,
-//                            NewsViewFragment.newInstance(bundle)
-//                        ).addToBackStack("NEWSVIEWFRAGMENT")
-//                    }
-//                }
-//                viewBinding.newsRecyclerView.adapter = newsDisplayAdapter
-//            }
-//
-//            override fun onFailure(call: Call<NewsListt.NewsList>, t: Throwable) {
-//
-//            }
-//
-//        })
-    }
-    private fun initObservers() {
-        newsFeedViewModel.newsArticleLiveData.observe(viewLifecycleOwner, Observer {
-            newsDisplayAdapter = NewsDisplayAdapter(it, requireContext()) {
-                val bundle = Bundle()
-                bundle.putSerializable(NewsViewFragment.NEWS_LIST_ITEM, it)
-                (activity as MainActivity).supportFragmentManager.commit {
-                    replace(
-                        R.id.fragment_container,
-                        NewsViewFragment.newInstance(bundle)
-                    ).addToBackStack("NEWSVIEWFRAGMENT")
+        val call: Call<NewsListt.NewsList> = ApiClient.getClient().create(ApiInterface::class.java)
+                        .getLatestNews("us","politics", API_KEY)
+        call.enqueue(object : Callback<NewsListt.NewsList>{
+            override fun onResponse(call: Call<NewsListt.NewsList>, response: Response<NewsListt.NewsList>) {
+                newsList = response.body()
+                newsDisplayAdapter = NewsDisplayAdapter(newsList!!, requireContext()) {
+                    val bundle = Bundle()
+                    bundle.putSerializable(NewsViewFragment.NEWS_LIST_ITEM, it)
+                    (activity as MainActivity).supportFragmentManager.commit {
+                        replace(
+                            R.id.fragment_container,
+                            NewsViewFragment.newInstance(bundle)
+                        ).addToBackStack("NEWSVIEWFRAGMENT")
+                    }
                 }
+                viewBinding.newsRecyclerView.adapter = newsDisplayAdapter
             }
-            viewBinding.newsRecyclerView.adapter = newsDisplayAdapter
+
+            override fun onFailure(call: Call<NewsListt.NewsList>, t: Throwable) {
+
+            }
+
         })
+    }
+    private fun initObservers(newsFeedViewModel: NewsFeedViewModel) {
+        lifecycleScope.launchWhenStarted {
+            newsFeedViewModel._newsArticleLiveData.observe(viewLifecycleOwner, Observer {
+                newsDisplayAdapter = NewsDisplayAdapter(it, requireContext()) {
+                    val bundle = Bundle()
+                    bundle.putSerializable(NewsViewFragment.NEWS_LIST_ITEM, it)
+                    (activity as MainActivity).supportFragmentManager.commit {
+                        replace(
+                            R.id.fragment_container,
+                            NewsViewFragment.newInstance(bundle)
+                        ).addToBackStack("NEWSVIEWFRAGMENT")
+                    }
+                }
+                viewBinding.newsRecyclerView.adapter = newsDisplayAdapter
+            })
+        }
     }
 }
--- byjusassignment/app/src/main/java/com/byjus/assignment/byjusAssignment/fragment/NewsFeedViewModel.kt
+++ byjusassignment/app/src/main/java/com/byjus/assignment/byjusAssignment/fragment/NewsFeedViewModel.kt
@@ -3,47 +3,86 @@ package com.byjus.assignment.byjusAssignment.fragment
 import android.app.Application
 import android.util.Log
 import androidx.lifecycle.*
+import com.byjus.assignment.byjusAssignment.database.NewsArticle
 import com.byjus.assignment.byjusAssignment.database.NewsArticlesDao
 import com.byjus.assignment.byjusAssignment.model.NewsListt
 import com.byjus.assignment.byjusAssignment.rest.ApiClient
 import com.byjus.assignment.byjusAssignment.rest.ApiInterface
 import com.google.gson.Gson
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import retrofit2.await
 import retrofit2.awaitResponse
 
-class NewsFeedViewModel(/*val database: NewsArticlesDao,
-                        application: Application*/)
-    :ViewModel() {
+class NewsFeedViewModel(val database: NewsArticlesDao, application: Application) :ViewModel() {
 
-    private var _newsArticleLiveData = MutableLiveData<NewsListt.NewsList>()
+    var _newsArticleLiveData = MutableLiveData<NewsListt.NewsList>()
     val newsArticleLiveData: LiveData<NewsListt.NewsList> get() = _newsArticleLiveData
+    var _newsArticlesDb = MutableLiveData<List<NewsArticle>>()
+    val newsArticlesDb : LiveData<List<NewsArticle>> get() = _newsArticlesDb
 
-//    init {
-//        getArticles("us", "politics", API_KEY)
-//    }
-        fun getArticles(country: String, category: String, apiKey: String) {
-            viewModelScope.launch {
+
+    fun getArticles(country: String, category: String, apiKey: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            Log.d("API FETCH", "API FETCH STARTED")
+
+            val response = ApiClient.getClient().create(ApiInterface::class.java)
+                .getLatestNews(country, category, apiKey).awaitResponse()
+            if (response.isSuccessful) {
+                val result = (response.body() as NewsListt.NewsList)
                 Log.d("API FETCH", "API FETCH SUCCESSFUL")
-                withContext(Dispatchers.IO) {
-
-
-                    val response = ApiClient.getClient().create(ApiInterface::class.java)
-                        .getLatestNews(country, category, apiKey).awaitResponse()
-                    if (response.isSuccessful) {
-                        val result = (response.body() as NewsListt.NewsList)
-                        Log.d("API FETCH", "API FETCH SUCCESSFUL")
-                        print(Gson().toJson(response))
-                        _newsArticleLiveData.postValue(result)
-                    }
-                }
+                _newsArticleLiveData.postValue(result)
             }
         }
+    }
+
+    val job = Job()
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+
+    private val uiScope = CoroutineScope(Dispatchers.Main + job)
+
+    fun insertArticlesIntoDB(article: NewsArticle) {
+        uiScope.launch {
+            insert(article)
+        }
+    }
+
+    private suspend fun insert(article: NewsArticle) {
+        withContext(Dispatchers.IO) {
+            database.insertArticle(article)
+        }
+    }
+
+    fun clearDb() {
+        uiScope.launch {
+            clearNewsDb()
+        }
+    }
+
+    private suspend fun clearNewsDb() {
+        withContext(Dispatchers.IO) {
+            database.clearDataBase()
+        }
+    }
+
+    fun getAllArticles() {
+         uiScope.launch {
+             val list = getAllArticlesDb()
+             _newsArticlesDb.postValue(list?.value)
+         }
+    }
+
+    private suspend fun getAllArticlesDb(): LiveData<List<NewsArticle>>? {
+        return withContext(Dispatchers.IO) {
+            val list = database.getAllArticles()
+            list
+        }
+    }
 
 }
 
-//class NewsFeedViewModelFactory(private val database: NewsArticlesDao, private val application: Application): ViewModelProvider.NewInstanceFactory() {
-//    override fun <T : ViewModel?> create(modelClass: Class<T>): T = NewsFeedViewModel(database, application) as T
-//}
+class NewsFeedViewModelFactory(private val database: NewsArticlesDao, private val application: Application): ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T = NewsFeedViewModel(database, application) as T
+}
